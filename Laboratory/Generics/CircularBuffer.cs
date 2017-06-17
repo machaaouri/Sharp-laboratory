247cc19bec203fb176c1f815932e43fd76beecd1
Houssame Machaaouri <hmachaao@gmail.com>
2017-06-17 20:37:59 +0200

    inherit from IBuffer

--- Laboratory/Generics/CircularBuffer.cs
+++ Laboratory/Generics/CircularBuffer.cs
@@ -3,6 +3,23 @@
 
     public class CircularBuffer<T> : Buffer<T>
     {
+        int _capacity;
 
+        public CircularBuffer(int capacity = 5)
+        {
+            _capacity = capacity;
+        }
+
+        public override void Write(T value)
+        {
+            base.Write(value);
+            if(_queue.Count > _capacity)
+            {
+                _queue.Dequeue();
+            }
+        }
+
+        public bool IsFull {  get { return _queue.Count == 0; } }
+        public int Count { get { return _queue.Count; } }
     }
 }
--- Laboratory/Generics/Program.cs
+++ Laboratory/Generics/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Generics
+﻿using System;
+
+namespace Generics
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            var buffer = new CircularBuffer<double>(3);
+            buffer.Write(1);
+            buffer.Write(2);
+            buffer.Write(3);
+            buffer.Write(4);
+
+            Console.WriteLine("Buffer Count : {0}", buffer.Count);
+            Console.ReadKey();
+
         }
     }
 }
