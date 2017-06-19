0c90505fccf1117136125221c3a2a5a4caa18e1d
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-19 15:01:14 +0200

    Implete Delete and findById

--- Laboratory/Constraints/Employee.cs
+++ Laboratory/Constraints/Employee.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace Constraints
 {
-    public class Employee : Person
+    public class Employee : Person, IEntity
     {
         public int Id { get; set; }
         public virtual void DoWork()
         {
             Console.Write("Doing real work");
+        }
+
+
+        public bool IsValid()
+        {
+            return true;
         }
     }
 }
