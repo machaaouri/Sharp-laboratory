35b96d0edc45342d6d1a664a66b641a7b6f76b67
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-08-22 11:37:29 +0200

    Generic LinkedList

---
+++ Laboratory/DataStructures/LinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Node<T>
+    {
+        Node<T> next = null;
+        T data;
+
+        public Node(T d) { data = d; }
+
+        public void Add_Tail(T d)
+        {
+            Node<T> end =  new Node<T>(d);
+            Node<T> n = this;
+
+            while(n.next != null) n = n.next;
+            n.next = end;
+        }
+
+        public void Display()
+        {
+            Node<T> n = this;
+            while(n != null)
+            {
+                Console.WriteLine(n.data);
+                n = n.next;
+            }
+        }
+    }
+}
---
+++ Laboratory/DataStructures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Node_Ex()
+        {
+            Node<int> node = new Node<int>(5);
+            node.Add_Tail(6);
+            node.Add_Tail(9);
+
+            node.Display();
+        }
+        static void Main(string[] args)
+        {
+            Node_Ex();
+            Console.ReadKey();
+        }
+    }
+}
