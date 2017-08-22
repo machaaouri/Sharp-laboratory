c6331d52e566ab4e0733ebe4dcf92bda9a06a2d5
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-08-22 12:39:51 +0200

    Add and a delete a node from the linkedList

--- Laboratory/DataStructures/LinkedList.cs
+++ Laboratory/DataStructures/LinkedList.cs
@@ -13,13 +13,41 @@ namespace DataStructures
 
         public Node(T d) { data = d; }
 
-        public void Add_Tail(T d)
+        public override bool Equals(object obj)
+        {
+            var comparer = Comparer<T>.Default;
+            T value = (T)obj;
+            return comparer.Compare(data, value) == 0;
+        }
+
+        public void addNodeToTail(T d)
         {
             Node<T> end =  new Node<T>(d);
             Node<T> n = this;
 
             while(n.next != null) n = n.next;
             n.next = end;
+        }
+
+        public Node<T> deteleNode(Node<T> head, T d)
+        {
+            Node<T> n = head;
+
+            if(n.data.Equals(d))
+            {
+                return head.next; // removed head
+            }
+
+            while(n.next != null)
+            {
+                if(n.next.Equals(d))
+                {
+                    n.next = n.next.next;
+                    break;
+                }
+                n = n.next;
+            }
+            return head;
         }
 
         public void Display()
--- Laboratory/DataStructures/Program.cs
+++ Laboratory/DataStructures/Program.cs
@@ -11,9 +11,12 @@ namespace DataStructures
         static void Node_Ex()
         {
             Node<int> node = new Node<int>(5);
-            node.Add_Tail(6);
-            node.Add_Tail(9);
+            node.addNodeToTail(6);
+            node.addNodeToTail(9);
+            node.Display();
 
+            Console.WriteLine("----------");
+            node = node.deteleNode(node, 6);
             node.Display();
         }
         static void Main(string[] args)
