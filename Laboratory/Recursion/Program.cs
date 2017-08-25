7223a076d46078892e69e697cacf2b80e68fa1d5
Houssame Machaaouri <hmachaao@gmail.com>
2017-08-25 14:10:03 +0200

    generate the nth fibonacci number

---
+++ Laboratory/Recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static int Fibo()
+        {
+            return Recursion.Fibonacci(25);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibo());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratory/Recursion/Recursion.cs
@@ -0,0 +1,14 @@
+﻿namespace Recursion
+{
+    public class Recursion
+    {
+        //enerate the nth fibonacci number
+        public static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else if (n > 1) return Fibonacci(n - 1) + Fibonacci(n - 2);
+            else return -1;
+        }
+    }
+}
