ecfafd43f22d29fdd47ac5f8bace5151d24d89d5
Houssame Machaaouri <hmachaao@gmail.com>
2017-08-26 12:33:17 +0200

    Merge 2 sorted arrays

---
+++ Laboratory/SortAndSearch/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SortAndSearch
+{
+    class Program
+    {
+        static void Merge()
+        {
+            int[] A = { 10, 21, 33, 35, 40, 0, 0, 0, 0, 0 };
+            int[] B = { 32, 34, 36, 41 };
+            SortAndSearch.merge(A, 5, B);
+        }
+        static void Main(string[] args)
+        {
+            Merge();
+        }
+    }
+}
---
+++ Laboratory/SortAndSearch/SortAndSearch.cs
@@ -0,0 +1,31 @@
+﻿namespace SortAndSearch
+{
+    public class SortAndSearch
+    {
+
+        // Given 2 arrays sorted , write method to merge B into A in sorted order.
+        public static void merge(int[] A,int M,int[] B)
+        {
+            int k = M + B.Length - 1;
+            int i = M - 1; // last element of A
+            int j = B.Length - 1; // last element of B
+
+            while(i>= 0  && j >= 0)
+            {
+                if(A[i] > B[j])
+                {
+                    A[k--] = A[i--];
+                }
+                else
+                {
+                    A[k--] = B[j--];
+                }
+            }
+            while(j>=0)
+            {
+                A[k--] = B[j--];
+            }
+        }
+
+    }
+}
