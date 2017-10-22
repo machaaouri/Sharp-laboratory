74d9b0bb44583fcdcdc35febc5139b9326dcddc0
Houssame Machaaouri <hmachaao@gmail.com>
2017-10-22 21:18:35 +0200

    Add a CSV repository

---
+++ Interfaces/PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using PersonRepository.CSV;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ServiceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+            IPersonRepository repository =  new ServiceRepository();
+            var peopel = repository.GetPeople();
+            foreach (var person in peopel)
+                PersonListBox.Items.Add(person);
+
+            ShowRepositoryType(repository);
+        }
+
+        private void CSVFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+            IPersonRepository repository = new CSVRepository();
+            var peopel = repository.GetPeople();
+            foreach (var person in peopel)
+                PersonListBox.Items.Add(person);
+
+            ShowRepositoryType(repository);
+
+        }
+
+        private void SQLFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            PersonListBox.Items.Clear();
+        }
+
+        private void ShowRepositoryType(IPersonRepository repository)
+        {
+            MessageBox.Show(string.Format("Repository Type:\n{0}",
+                repository.GetType().ToString()));
+        }
+    }
+}
