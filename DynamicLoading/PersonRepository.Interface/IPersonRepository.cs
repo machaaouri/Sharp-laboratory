fc4a5f547b1765dbcc52f8d6d41f41bb49ed3ec9
Houssame Machaaouri <hmachaao@gmail.com>
2017-10-23 21:40:06 +0200

    Progrmming to an interface and then dynamically load the concrete types

---
+++ DynamicLoading/PersonRepository.Interface/IPersonRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace PersonRepository.Interface
+{
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> GetPeople();
+
+        Person GetPerson(string lastName);
+
+        void AddPerson(Person newPerson);
+
+        void UpdatePerson(string lastName, Person updatedPerson);
+
+        void UpdatePeople(IEnumerable<Person> updatePeople);
+
+        void DeletePerson(string lastName);
+
+        
+
+    }
+}
