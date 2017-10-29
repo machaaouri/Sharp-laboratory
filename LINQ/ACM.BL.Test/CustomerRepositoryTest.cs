6a4f0ecf42cfd62c155b37d8126b3a7d463df7b1
Houssame Machaaouri <hmachaao@gmail.com>
2017-10-29 21:48:30 +0100

    Practical LINQ

---
+++ LINQ/ACM.BL.Test/CustomerRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void FindExistingCustomer()
+        {
+            CustomerRepository repository = new CustomerRepository();
+
+            var customerList = repository.Retrieve();
+
+            var result = repository.Find(customerList, 2);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.CustomerId);
+            Assert.AreEqual("Baggins", result.LastName);
+            Assert.AreEqual("Bilbo", result.FirstName);
+        }
+    }
+}
