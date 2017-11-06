ce9660547fae6d44cc580529a749d085edf8d2e3
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-11-06 18:53:35 +0100

    Use of Anonymous method

--- DelegatesAndEvents/SimpleDelegate/Program.cs
+++ DelegatesAndEvents/SimpleDelegate/Program.cs
@@ -13,19 +13,22 @@ namespace SimpleDelegate
             {
                 var worker = new Worker();
 
-                worker.WorkPerformed += worker_workPerformed;
-                worker.WorkCompleted += woker_workCompleted;
+                worker.WorkPerformed += Worker_WorkPerformed;
+                worker.WorkCompleted += delegate(object sender, EventArgs e)
+                {
+                    Console.WriteLine("Work completed !"); // use an Anonymous Method
+                };
                 worker.Dowork(9,WorkType.PrintPapers);
 
                 Console.Read();
             }
 
-            static void woker_workCompleted(object sender, EventArgs e)
-            {
-                Console.WriteLine("Work completed !");
-            }
+            //static void Worker_WorkCompleted(object sender, EventArgs e)
+            //{
+            //    Console.WriteLine("Work completed !");
+            //}
 
-            static void worker_workPerformed(object sender, WorkPerformedEventArgs e)
+            static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
             {
                 Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
             }
